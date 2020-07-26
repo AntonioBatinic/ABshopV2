8f6c1b2dc2de3a2d4b4dbe5cd87d6a7bc8b908d9
AntonioBatinic <a.batinichr@gmail.com>
2020-07-26 18:47:28 +0200

    Basic frontend and backend

---
+++ frontend/src/data.js
@@ -0,0 +1,31 @@
+export default  {
+    products:[
+        {
+        id: '1',
+        name: "Raw mug",
+        category: 'Mugs',
+        image:"/images/mug1.png",
+        price: 15,
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        id: '2',
+        name: "Donut mug",
+        category: 'Mugs',
+        image:"/images/mug2.png",
+        price: 15,
+        rating: 4.2,
+        numReviews: 4
+    },
+    {
+        id: '3',
+        name: "Abstract vector mug",
+        category: 'Mugs',
+        image:"/images/mug3.png",
+        price: 15,
+        rating: 4.7,
+        numReviews: 10
+    },
+]
+}
